eeafd40b68878e51ea09fbfa2f39750409b3a544
ksdfg <ksdfg123@gmail.com>
2019-04-26 12:44:00 +0530

    hash420 : linear probing w/o replacement removed possible infinite loop

--- hash_420/hash_classes.cpp
+++ hash_420/hash_classes.cpp
@@ -12,10 +12,12 @@ using namespace std;
 
 void Hashing::lWoP(){
     Student meow;   //to take i/p of student
-    int index = 0;  //to store hash index of record to insert
+    int index = 0, nyan;  //to store hash index of record to insert
     bool shutdown = false;  //control outer loop : how many entries
     
     do{
+        nyan = 0;   //reset value
+        
         //take input of student
         cout<<endl<<"enter name of student : ";
         cin>>meow.name;
@@ -24,13 +26,21 @@ void Hashing::lWoP(){
         cout<<"enter grade of student : ";
         cin>>meow.grade;
         
-        index = meow.rollNo % MAX;  //calculate initial hash index
+        nyan = index = meow.rollNo % MAX;  //calculate initial hash index
         
         while(hashtable[index].rollNo != -1){  //calculating correct hash index
             index = ++index % MAX;
+            shutdown = true;
+            if(index == nyan)
+                break;
         }
         
-        hashtable[index] = meow;
+        if(index == nyan && shutdown){
+            cout<<endl<<"can't enter, no space"<<endl;
+        }
+        else{
+            hashtable[index] = meow;
+        }
         
         cout<<endl<<"do you want to enter another student? 1 or 0 : ";
         cin>>index; //using index to store i/p
