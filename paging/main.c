73d1a1f911e954746b91af04a966c754dbecd7fd
ksdfg <ksdfg123@gmail.com>
2019-04-29 19:39:37 +0530

    paging : fifo working

--- paging/main.c
+++ paging/main.c
@@ -18,34 +18,57 @@
  * 
  */
 struct queue{   //struct for queue
-    int arr[4];
+    int arr[3];
     int rear;
     int front;
 } typedef Queue;
 
 Queue makeQueue(){  //to initialize front and rear of a queue to 0
     Queue q;
+    for(int i=0; i<3; i++)  //initially set all to -1 (invalid)
+        q.arr[i] = -1;
     q.front = 0;
     q.rear = 0;
     return q;
 }
 
-void push(Queue q, int toPush){ //push item into queue
-    q.rear = (q.rear + 1) % 4;
-    q.arr[q.rear] = toPush;
+void push(Queue *q, int toPush){ //push item into queue
+    q->arr[q->rear] = toPush;
+    q->rear = (q->rear + 1) % 3;    //update front value circularly
 }
 
-int pop(Queue q){   //pop item from queue
-    int ret = q.arr[q.front];
-    q.front = (q.front + 1) % 4;
-    return ret;
-}
-
-int fifo(int ref[]){    //find number of page faults
-    int fault = 0;
-    Queue q = makeQueue();
+int fifo(int ref[], int refLen){    //find number of page faults
+    int fault = 0, meow = 0;    //meow is temp, fault stores number of page faults
+    Queue q = makeQueue();      //queue to store page frames
     
-    return fault;
+    for(int i=0; i<refLen; i++){    //traverse through reference string
+        meow = 0;
+        
+        printf("\n%d", ref[i]); //current page number
+        
+        for(int j=0; j<3; j++)
+            if(q.arr[j] == ref[i]){ //if page already in one of the frames
+                meow = 1;
+                break;
+            }
+        
+        if(meow)
+            continue;    //go to next page number
+        
+        push(&q, ref[i]);   //push current page number into queue (call it to p.m.)
+        
+        printf("\t:\t");
+        for(int i=0; i<3; i++){ //print pages mapped to each frame
+            if(q.arr[i] == -1)
+                printf("-\t");
+            else
+                printf("%d\t", q.arr[i]);
+        }
+        
+        fault++;
+    }
+    
+    return fault;   //kaeritai
 }
 
 int main(int argc, char** argv) {
@@ -58,6 +81,8 @@ int main(int argc, char** argv) {
     printf("\nEnter reference string :\n");
     for(int i=0; i<refLen; i++)
         scanf("%d", &ref[i]);   //taking input of reference string
+    
+    printf("\n\nfaults = %d", fifo(ref, refLen));   //call fifo, print number of page faults
 
     return (EXIT_SUCCESS);
 }
